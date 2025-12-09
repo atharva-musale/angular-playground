2ebf1ee0c5677bf7b2531cf526903f25e4618e62
Atharva <atharvamilind.musale@amadeus.com>
2025-12-09 12:01:41 +0530

    feat: upgrade to angular 21

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,11 +3,11 @@ import { Observable } from 'rxjs';
 import { User } from '../../models';
 import { UserDataService } from '../../services';
 import { UserNamePipe } from '../../pipes';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [UserNamePipe, NgIf, AsyncPipe],
+  imports: [UserNamePipe, AsyncPipe],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
   standalone: true,
