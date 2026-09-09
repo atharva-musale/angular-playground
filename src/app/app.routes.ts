f260cb958059f8b59c8c63d1d0bfda2d8360894a
Atharva <atharvamilind.musale@amadeus.com>
2026-09-09 17:14:39 +0530

    feat: components to feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 import { FormCheckGuard } from './guards';
 
 export const routes: Routes = [
@@ -14,23 +14,23 @@ export const routes: Routes = [
   },
   {
     path: 'users',
-    loadComponent: () => import('./components/users/user-page/user-list.component').then(m => m.UserListComponent),
+    loadComponent: () => import('./features/users/user-page/user-list.component').then(m => m.UserListComponent),
     children: [
       {
         path: ':id',
-        loadComponent: () => import('./components/users/user/user.component').then(m => m.UserComponent)
+        loadComponent: () => import('./features/users/user/user.component').then(m => m.UserComponent)
       }
     ]
   },
   {
     path: 'add-new-user',
     canDeactivate: [FormCheckGuard],
-    loadComponent: () => import('./components/add-new-user/add-new-user.component').then(m => m.AddNewUserComponent)
+    loadComponent: () => import('./features/add-new-user/add-new-user.component').then(m => m.AddNewUserComponent)
   },
   {
     path: 'sticky-ball-counter',
     loadComponent: () => import(
-      './components/sticky-ball-counter/sticky-ball-counter-page/sticky-ball-counter-page.component'
+      './features/sticky-ball-counter/sticky-ball-counter-page/sticky-ball-counter-page.component'
     ).then(m => m.StickyBallCounterPageComponent)
   }
 ];
